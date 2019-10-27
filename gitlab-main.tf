aa94f64a2b556ad293e3b662407f3a2d668f46df
Apollo Akora <apolloakora@gmail.com>
2019-10-27 16:55:33 +0000

    moving the resources towards terraform 12

--- gitlab-main.tf
+++ gitlab-main.tf
@@ -1,35 +1,52 @@
 
+/*
+ | --
+ | -- If you are using an IAM role as the AWS access mechanism then
+ | -- pass it as in_role_arn commonly through an environment variable
+ | -- named TF_VAR_in_role_arn in addition to the usual AWS access
+ | -- key, secret key and default region parameters.
+ | --
+*/
+provider aws {
+    dynamic assume_role {
+        for_each = length( var.in_role_arn ) > 0 ? [ var.in_role_arn ] : [] 
+        content {
+            role_arn = assume_role.value
+	}
+    }
+}
+
+
 ### ---> ###################### <--- ### || < ####### > || ###
 ### ---> ---------------------- <--- ### || < ------- > || ###
 ### ---> Instance Layer Modules <--- ### || < Layer I > || ###
 ### ---> ---------------------- <--- ### || < ------- > || ###
 ### ---> ###################### <--- ### || < ####### > || ###
 
-module ec2-instance
-{
+module ec2-instance {
     source                  = "github.com/devops4me/terraform-aws-ec2-instance-cluster"
 
     in_node_count           = "1"
-    in_user_data            = "${ data.template_file.cloud_config.rendered }"
-    in_iam_instance_profile = "${ module.ec2-instance-profile.out_ec2_instance_profile }"
-    in_ssh_public_key       = "${ var.in_ssh_public_key }"
+    in_user_data            = data.template_file.cloud_config.rendered
+    in_iam_instance_profile = module.ec2-instance-profile.out_ec2_instance_profile
+    in_ssh_public_key       = var.in_ssh_public_key
 
-    in_ami_id               = "${ data.aws_ami.ubuntu-1804.id }"
-    in_subnet_ids           = [ "${ element ( module.sub-network.out_public_subnet_ids, 0 ) }" ]
-    in_security_group_ids   = [ "${ module.security-group.out_security_group_id }" ]
+    in_ami_id               = data.aws_ami.ubuntu-1804.id
+    in_subnet_ids           = [ element ( module.sub-network.out_public_subnet_ids, 0 ) ]
+    in_security_group_ids   = [ module.security-group.out_security_group_id ]
 
-    in_ecosystem_name       = "${ var.in_ecosystem_name }"
-    in_tag_timestamp        = "${ module.resource-tags.out_tag_timestamp }"
-    in_tag_description      = "${ module.resource-tags.out_tag_description }"
+    in_ecosystem   = local.ecosystem_name
+    in_timestamp   = local.timestamp
+    in_description = local.description
 }
 
-module ec2-instance-profile
-{
-    source             = "github.com/devops4me/terraform-aws-ec2-instance-profile"
-    in_policy_stmts    = "${ data.template_file.iam_policy_stmts.rendered }"
-    in_ecosystem_name  = "${ var.in_ecosystem_name }"
-    in_tag_timestamp   = "${ module.resource-tags.out_tag_timestamp }"
-    in_tag_description = "${ module.resource-tags.out_tag_description }"
+
+module ec2-instance-profile {
+
+    source = "github.com/devops4me/terraform-aws-ec2-instance-profile"
+
+    in_policy_stmts = data.template_file.iam_policy_stmts.rendered
+
 }
 
 
@@ -39,35 +56,44 @@ module ec2-instance-profile
 ### ---> --------------------- <--- ### || < ------- > || ###
 ### ---> ##################### <--- ### || < ####### > || ###
 
-module sub-network
-{
-    source            = "github.com/devops4me/terraform-aws-sub-network"
-    in_vpc_id         = "${ var.in_vpc_id }"
-    in_vpc_cidr       = "${ var.in_vpc_cidr }"
-    in_net_gateway_id = "${ var.in_gateway_id }"
-    in_subnets_max    = "${ var.in_subnets_max }"
-    in_subnet_offset  = "${ var.in_subnet_offset }"
+module vpc-network {
 
-    in_num_public_subnets   = 1
-    in_num_private_subnets  = 0
+    source  = "devops4me/vpc-network/aws"
+    version = "~> 1.0.2"
 
-    in_ecosystem_name      = "${ var.in_ecosystem_name }"
-    in_tag_timestamp       = "${ module.resource-tags.out_tag_timestamp }"
-    in_tag_description     = "${ module.resource-tags.out_tag_description }"
+    in_vpc_cidr            = "10.82.0.0/16"
+    in_num_public_subnets  = 1
+    in_num_private_subnets = 0
+
+    in_ecosystem   = local.ecosystem_name
+    in_timestamp   = local.timestamp
+    in_description = local.description
 }
 
-module security-group
-{
-    source      = "github.com/devops4me/terraform-aws-security-group"
-    in_ingress  = [ "http", "https", "ssh" ]
-    in_vpc_id   = "${ var.in_vpc_id }"
 
-    in_ecosystem_name  = "${ var.in_ecosystem_name }"
-    in_tag_timestamp   = "${ module.resource-tags.out_tag_timestamp }"
-    in_tag_description = "${ module.resource-tags.out_tag_description }"
+module security-group {
+
+    source     = "github.com/devops4me/terraform-aws-security-group"
+    in_ingress = [ "postgres", "ssh" ]
+    in_vpc_id  = module.vpc-network.out_vpc_id
+
+    in_ecosystem   = local.ecosystem_name
+    in_timestamp   = local.timestamp
+    in_description = local.description
 }
 
-module resource-tags
-{
-    source = "github.com/devops4me/terraform-aws-resource-tags"
+################################################################################################
+################################################################################################
+################################################################################################
+
+
+locals {
+
+    fresh_db_name  = "brandnewdb"
+    clone_db_name  = "snapshotdb"
+
+    ecosystem_name = "sanjievesdb"
+    timestamp = formatdate( "YYMMDDhhmmss", timestamp() )
+    date_time = formatdate( "EEEE DD-MMM-YY hh:mm:ss ZZZ", timestamp() )
+    description = "was created by me on ${ local.date_time }."
 }
